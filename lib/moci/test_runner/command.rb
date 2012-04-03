601423ba6288a9176a20834bb364c73d26d596aa
comboy <kacper.ciesla@gmail.com>
2012-04-03 12:35:26 +0200

    add pre and post command options for command runner

--- lib/moci/test_runner/command.rb
+++ lib/moci/test_runner/command.rb
@@ -4,12 +4,16 @@ module Moci
     # Simplest runner to execute arbitrary commands
     # It only cares about exit code
     # Options:
-    # * command  - command to execute
+    # * *command*  - command to execute, which exit code is taken as a result
+    # * pre_command - optional command to execute before
+    # * post_command - optional command to execute after (no matter what result was)
     class Command < Base
       def run
         t0 = Time.now
         output = ''
+        execute(options['pre_command', output) if options['pre_command']
         exitstatus = execute(options['command'], output)
+        execute(options['post_command', output) if options['post_command']
         run_time = Time.now - t0
         push(
           :exitstatus => exitstatus,
