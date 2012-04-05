737a81ac1a7abef7a86bdaa8d51dbd4ac0f21c92
comboy <kacper.ciesla@gmail.com>
2012-04-05 22:26:37 +0200

    more graphs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,16 +8,30 @@ class ProjectsController < ApplicationController
 
   def stats
     must_be_in_project
-    @assertions = {}
+    @assertions = []
+    @tests_failing = []
+    @run_times = []
     @project.test_suites.each do |ts|
       # IMPROVE: N+!, but it's stats, we can cache
       data = @project.commits.order('committed_at DESC').limit(500).map do |commit|
-        number = TestSuiteRun.where(:commit_id => commit.id, :test_suite_id => ts.id).average(:assertions_count).to_i
-        number == 0 ? nil : number
+        r = TestSuiteRun.select('max(assertions_count) assertions_count, min(errors_count) errors_count ,min(failures_count) failures_count, avg(run_time) run_time').where(:commit_id => commit.id, :test_suite_id => ts.id).first
+        r['assertions_count'] = nil if r['assertions_count'] == 0
+        r['total_errors'] = r['errors_count'].to_i + r['failures_count'].to_i
+        r['run_time'] = nil if r['run_time'] == 0
+        r
       end
-      data_with_index = []
-      data.compact.reverse.each_with_index { |d,i| data_with_index << [i,d] }
-      @assertions[ts.name] = {:label => ts.name, :data => data_with_index}
+
+      @assertions << {:label => ts.name, :data => flot_data(data,'assertions_count')}
+      @tests_failing << {:label => ts.name, :data => flot_data(data,'total_errors')}
+      @run_times << {:label => ts.name, :data => flot_data(data,'run_time')}
     end
+  end
+
+  protected
+
+  def flot_data(data, value)
+    data_with_index = []
+    data.compact.reverse.each_with_index { |d,i| data_with_index << [i,d[value]] }
+    data_with_index
   end
 end
