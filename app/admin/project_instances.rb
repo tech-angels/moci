89f2621bce89d0ce3647f94551c81f138e6b2bd4
comboy <kacper.ciesla@gmail.com>
2012-05-20 20:13:29 +0200

    update project instance admin form

--- app/admin/project_instances.rb
+++ app/admin/project_instances.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register ProjectInstance do
   menu :parent => "Projects", :label => "Instances"
+
+  form do |f|
+    f.inputs do
+      f.input :project
+      f.input :working_directory
+    end
+    
+    f.buttons
+  end
 end
