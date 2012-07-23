21d744fa36dc8cd5894bf98c2e60db97fdeff8b6
comboy <kacper.ciesla@gmail.com>
2012-07-23 12:03:02 +0200

    add Ruby project handler (between Rails and Base handlers)

--- lib/moci/project_handler/rails.rb
+++ lib/moci/project_handler/rails.rb
@@ -5,32 +5,7 @@ module Moci
     # TODO this is was mostly thinking about rails 3+, we should add compatibility for older versions too
     # Options: (project_instance.options[:rails]
     # * db_structure_dump - should database structure be preseverved between commits (default true)
-    # * rvm - if set, it is assummed rvm with given ruby version is installed, and it will be used
-    #         for all commands executed within project instance (example value: 1.8.7@moci)
-    # * bundler - if true, means project is using bundler (default true)
-    class Rails < Base
-
-      define_options do
-        o :bundler,  "Does this project use bundler?", :type => :boolean, :default => true
-        o :rvm,       "If set, it is assummed rvm with given ruby version is installed, and it will be used"\
-                      "for all commands executed within project instance (example value: 1.8.7@moci)", :name => "RVM"
-      end
-
-      def execute_wrapper(command, output='')
-        if options[:bundler]
-          command = "bundle exec #{command}" unless command.match(/bundle (install|check|exec)/)
-          command = "BUNDLE_GEMFILE=\"Gemfile\" && #{command}"
-        end
-
-        if options[:rvm]
-          # It totally sucks that we have to load bash just for RVM
-          # TODO: http://beginrescueend.com/workflow/scripting/ (use global installation if present
-          # TODO we should check if ruby is installed first, and raise some exceptions
-          command = "bash -c 'source \"$HOME/.rvm/scripts/rvm\" && rvm #{options[:rvm]} && #{command}'"
-        end
-
-        Bundler.with_clean_env { yield(command, output) }
-      end
+    class Rails < Ruby
 
       def prepare_env(commit)
         output = ''
