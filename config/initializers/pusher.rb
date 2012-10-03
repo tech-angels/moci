2f5e8eedc11e9a2ccc4ff4d4eb71d46ec373c48d
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2012-10-03 16:41:16 +0200

    Fix pusher initializer

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,9 @@
 Pusher.logger = Rails.logger
-Pusher.encrypted  = Moci.config[:pusher][:encrypted] == "true" ? true : false
-Pusher.host       = Moci.config[:pusher][:host]
-Pusher.port       = Moci.config[:pusher][:port]
-Pusher.app_id     = Moci.config[:pusher][:app_id]
-Pusher.key        = Moci.config[:pusher][:key]
-Pusher.secret     = Moci.config[:pusher][:secret]
+if Moci.config[:pusher]
+  Pusher.encrypted  = Moci.config[:pusher][:encrypted] == "true" ? true : false if Moci.config[:pusher][:encrypted]
+  Pusher.host       = Moci.config[:pusher][:host] if Moci.config[:pusher][:host]
+  Pusher.port       = Moci.config[:pusher][:port] if Moci.config[:pusher][:port]
+  Pusher.app_id     = Moci.config[:pusher][:app_id]
+  Pusher.key        = Moci.config[:pusher][:key]
+  Pusher.secret     = Moci.config[:pusher][:secret]
+end
