4542977784db5c199964592276931adc0dc7d9c9
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2012-10-07 20:09:59 +0200

    Quick fix for different rvm install types

--- lib/moci/project_handler/ruby.rb
+++ lib/moci/project_handler/ruby.rb
@@ -9,8 +9,8 @@ module Moci
     class Ruby < Base
       define_options do
         o :bundler,  "Does this project use bundler?", :type => :boolean, :default => true
-        o :rvm,       "If set, it is assummed rvm with given ruby version is installed, and it will be used"\
-                      "for all commands executed within project instance (example value: 1.8.7@moci)", :name => "RVM"
+        o :rvm,      "If set, it is assummed rvm with given ruby version is installed, and it will be used"\
+                     "for all commands executed within project instance (example value: 1.8.7@moci)", :name => "RVM"
       end
 
       def execute_wrapper(command, output='')
@@ -23,7 +23,9 @@ module Moci
           # It totally sucks that we have to load bash just for RVM
           # TODO: http://beginrescueend.com/workflow/scripting/ (use global installation if present
           # TODO we should check if ruby is installed first, and raise some exceptions
-          command = "bash -c 'source \"$HOME/.rvm/scripts/rvm\" && rvm #{options[:rvm]} && #{command}'"
+          # TODO: Add rvm_type to dynamic options
+          rvm_load = Moci.config[:rvm_source] || "$HOME/.rvm/scripts/rvm"
+          command = "bash -c 'source \"#{rvm_load}\" && rvm #{options[:rvm]} && #{command}'"
         end
 
         Bundler.with_clean_env { yield(command, output) }
