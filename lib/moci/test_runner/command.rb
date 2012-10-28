9e75dc7ff9af9c6fab62935ec0a2449405c32752
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2012-10-28 13:18:46 +0100

    Don't execute pre/post_command if blank

--- lib/moci/test_runner/command.rb
+++ lib/moci/test_runner/command.rb
@@ -14,9 +14,9 @@ module Moci
       def run
         t0 = Time.now
         output = ''
-        execute(options['pre_command'], output) if options['pre_command']
+        execute(options['pre_command'], output) unless options['pre_command'].blank?
         exitstatus = execute(options['command'], output)
-        execute(options['post_command'], output) if options['post_command']
+        execute(options['post_command'], output) unless options['post_command'].blank?
         run_time = Time.now - t0
         push(
           :exitstatus => exitstatus,
